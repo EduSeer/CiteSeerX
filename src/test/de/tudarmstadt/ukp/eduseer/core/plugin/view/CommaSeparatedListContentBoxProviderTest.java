8e1dad657971b37be5eebf4e1bd08cbeb9476a82
Roland Kluge <roland.kluge@gmx.de>
2013-07-30 22:12:17 +0200

    Add plugin files

---
+++ src/test/de/tudarmstadt/ukp/eduseer/core/plugin/view/CommaSeparatedListContentBoxProviderTest.java
@@ -0,0 +1,13 @@
+package de.tudarmstadt.ukp.eduseer.core.plugin.view;
+
+/**
+ * Test cases for {@link CompactListContentBoxProvider}.
+ *
+ * @author Roland Kluge
+ *
+ */
+public class CommaSeparatedListContentBoxProviderTest
+{
+
+    //TODO rkluge: implement me
+}
